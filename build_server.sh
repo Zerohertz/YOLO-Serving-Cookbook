735153841b713590659b83d10f899d7759b22579
Zerohertz <ohg3417@gmail.com>
2023-10-25 09:58:33 +0900

    :bento: Update: Ingress

--- build_server.sh
+++ build_server.sh
@@ -1,4 +1,4 @@
 kubectl create namespace yolo
 kubectl apply -f triton-inference-server.yaml -n yolo
 kubectl apply -f fastapi.yaml -n yolo
-# kubectl apply -f ingress.yaml -n yolo
+kubectl apply -f ingress.yaml -n yolo
