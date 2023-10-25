c235a2415e48d138a9ff5c5990891af763c84f4f
Zerohertz <ohg3417@gmail.com>
2023-10-25 09:22:19 +0900

    :sparkles: Feat: Evaluation

--- curl.py
+++ curl.py
@@ -6,7 +6,9 @@ from concurrent.futures import ThreadPoolExecutor
 import cv2
 import requests
 
+FILE_NAME = "tmp"
 SERIAL = True
+RANDOM = False
 
 URL = "http://zerohertz.xyz:80/"
 
@@ -16,36 +18,50 @@ HEADERS = {
 }
 
 
-def send_request(HEADERS, DATA):
-    time.sleep(random.randrange(0, 20))
+def send_request(HEADERS, DATA, RANDOM=False):
+    if RANDOM:
+        time.sleep(random.randrange(0, 20))
     response = requests.post(URL, headers=HEADERS, json=DATA, verify=False)
     res = response.json()
     print(
         f"""TOTAL: {res["process_time"]["total"]:.2f} | PREPROCESS: {res["process_time"]["preprocess"]:.2f} | INFERENCE: {res["process_time"]["inference"]:.2f} | POSTPROCESS: {res["process_time"]["postprocess"]:.2f} | VISUALIZE: {res["process_time"]["visualize"]:.2f}"""
     )
     print(res["results"][0])
-    return response
+    return str(res["process_time"]["total"]) + "\n"
 
 
-if __name__ == "__main__":
+def main(SESSION):
     file_id = "test.jpg"
     img = cv2.imread(file_id)
     _, buffer = cv2.imencode(".jpg", img)
     DATA = {
         "img": base64.b64encode(buffer).decode("utf-8"),
     }
-    max_workers = 100
+    max_workers = 10
 
     START = time.time()
     if SERIAL:
+        responses = []
         for _ in range(max_workers):
-            send_request(HEADERS, DATA)
+            responses.append(send_request(HEADERS, DATA))
     else:
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             responses = list(
                 executor.map(
-                    send_request, [HEADERS] * max_workers, [DATA] * max_workers
+                    send_request,
+                    [HEADERS] * max_workers,
+                    [DATA] * max_workers,
+                    [RANDOM] * max_workers,
                 )
             )
     END = time.time()
     print("=" * 10, END - START, "=" * 10)
+    with open(f"{FILE_NAME}_{SESSION}.txt", "w") as f:
+        f.writelines(responses)
+        f.writelines(str(END - START) + "\n")
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        main(i)
+        time.sleep(10)
